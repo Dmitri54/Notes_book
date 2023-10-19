d14b4dbd1729161766687c303d9f1f6312b878a8
Dmitri54 <dmitrimining@gmail.com>
2023-10-19 18:59:48 +0700

    Made changes to the code

--- Models/Note.py
+++ Models/Note.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import uuid 
+# import uuid 
 import Controller.counter as counter
 
 
--- UI/userMenu.py
+++ UI/userMenu.py
@@ -3,7 +3,7 @@ def userConsole():
     printMenuTitle("Главное меню\n         Книга заметок")
     print("1 - Добавить \n2 - Редактировать \n3 - Вывод всех заметок \n4 - Поиск по ID" 
           " \n5 - Поиск по дате \n6 - Удалить \n7 - Выход")
-    printMenuEmtyLine()
+    printEmtyLine()
     print("Выберите пункт")
 
 
@@ -13,5 +13,5 @@ def printMenuTitle(textMenu):
     print("        ", textMenu)
     print("==============================")
 
-def printMenuEmtyLine():
+def printEmtyLine():
     print("==============================")
