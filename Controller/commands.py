c3d4d1fe5631edc85e9bd2b10fb97d1cac7c5a5c
Dmitri54 <dmitrimining@gmail.com>
2023-10-19 17:54:40 +0700

    Added counter

--- Controller/commands.py
+++ Controller/commands.py
@@ -1,9 +1,22 @@
+import Models.Note
+import Storage.loadFromFile as lF
+import Storage.writeToFile as wF
+
 
 def addNote():
-    return
+    title = input("Введите заголовок заметки:\n")
+    body = input("Введите описание заметки:\n")
+    note = Models.Note.Note(title=title, body=body)
+    array_notes = lF.read_file()
+    for i in array_notes:
+        if Models.Note.Note.get_id(note) == Models.Note.Note.get_id(i):
+            Models.Note.Note.set_id(node)
+    array_notes.append(note)
+    wF.write_file(array_notes, 'a')
+    print("Заметка добавлена")
 
 def showNote(txt):
-    return
+    array_notes = 
 
 def delNote():
     return
